e7aa2e02925853c6c7a5df3331a42d842eb425f1
Yuen Teng Lam <lamyuentenq@gmail.com>
2026-07-14 17:26:28 +0800

    Create clean architecture solution structure

--- SmartFactory.Domain/Entities/Machine.cs
+++ SmartFactory.Domain/Entities/Machine.cs
@@ -1,8 +1,8 @@
-namespace SmartFactory.Api.Models.Entities;
+namespace SmartFactory.Domain.Entities;
 
 public class Machine
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
 
     public string Name { get; set; } = string.Empty;
 
